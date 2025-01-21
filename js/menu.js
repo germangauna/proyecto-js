986f2f00534af6e5ba734bedf6c5d3c1dc105da2
GermanG <germangauna88@gmail.com>
2025-01-20 22:05:26 -0300

    mediaquery 800px

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,16 @@
 (function () {
-    const openButton = document.querySelector('.nav__menu');
+    const openBotton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
 
-    openButton.addEventListener('click', () => {
-        alert('ouch, me diste')
-    })
+    
+    openBotton.addEventListener('click',() => {
+        menu.classList.add('nav__link--show');
+    });
+    
+    closeMenu.addEventListener('click',() => {
+        menu.classList.remove('nav__link--show');
+    });
+
+
 })();
